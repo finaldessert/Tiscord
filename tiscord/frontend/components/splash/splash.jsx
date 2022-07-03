a439a6043d0adfebc245e994bfd6e7c6c4d9f8e3
Ben <finaldessert@gmail.com>
2022-07-03 15:40:05 -0400

    cloud background fixed

--- tiscord/frontend/components/splash/splash.jsx
+++ tiscord/frontend/components/splash/splash.jsx
@@ -29,14 +29,12 @@ class Splash extends React.Component {
                         </Link>
                     </div>
                     <div id='hero-container'>
-                        <div id='splash-container-1'>
-                            <div id='splash-body-container'>
+                        <div id='splash-body-container'>
                                 <div id='splash-body-header'> Imagine a place...</div>
                                 <div id='splash-body-body'> ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often. </div>
                                 <img src={clouds} id="clouds"/>
                                 <img src={homeleft} id='homeleft'/>
                                 <img src={homeright} id='homeright'/>
-                            </div>
                         </div>
                     </div>
             </div>
