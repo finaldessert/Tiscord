f2ef0591bce777fc6ec126252b49da134740f95b
Ben <finaldessert@gmail.com>
2022-07-03 14:03:42 -0400

    splash reset

--- tiscord/frontend/components/splash/splash.jsx
+++ tiscord/frontend/components/splash/splash.jsx
@@ -33,11 +33,9 @@ class Splash extends React.Component {
                             <div id='splash-body-container'>
                                 <div id='splash-body-header'> Imagine a place...</div>
                                 <div id='splash-body-body'> ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often. </div>
-                            </div>
-                            <div id='splash-body-image-container'>
-                                <img src={clouds} id="clouds" alt="clouds" />
-                                <img src={homeleft} id='homeleft' alt='homeleft' />
-                                <img src={homeright} id='homeright' alt="homeright" />
+                                <img src={clouds} id="clouds"/>
+                                <img src={homeleft} id='homeleft'/>
+                                <img src={homeright} id='homeright'/>
                             </div>
                         </div>
                     </div>
