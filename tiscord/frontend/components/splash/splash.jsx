813c700372ceaac3c0c8ec4d2e6439d4a38394bf
Ben <finaldessert@gmail.com>
2022-07-05 15:16:56 -0400

    Basic Landing Page Format Finished

--- tiscord/frontend/components/splash/splash.jsx
+++ tiscord/frontend/components/splash/splash.jsx
@@ -39,12 +39,8 @@ class Splash extends React.Component {
                     </div>
                     <div id='hero-container'>
                         <div id='splash-body-container'>
-                                <div id='splash-body-header'> Imagine a place...</div>
-                                <div id='splash-body-body'> ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often. </div>
-                        </div>
-                        <div id='background-container'>
-                                <img src={homeleft} id='homeleft'/>
-                                <img src={homeright} id='homeright'/>
+                                <h1 id='splash-body-header'> Imagine a place...</h1>
+                                <body id='splash-body-body'> ...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often. </body>
                         </div>
                     </div>
             </div>
