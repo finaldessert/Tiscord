be21ff9dea0d3f9609d7fef967d74517fddf2cc1
Ben <finaldessert@gmail.com>
2022-07-12 19:32:54 -0400

    routes added for friendscontroller

--- tiscord/app/controllers/api/friends_controller.rb
+++ tiscord/app/controllers/api/friends_controller.rb
@@ -3,7 +3,21 @@ class Api::FriendsController < ApplicationController
         @friends = Friend.where(:user_id => current_user.id) #we can use this to grab all friends of current user for friends nav bar
     end
 
+    def show
+    end
+
     def create
-        
+        @friend = current_user.friends.build(friend_params)
+        if @friend.save
+            flash[:success] = ["Successfully added"]
+        else
+            render json: ["Error, Try again later"]
+        end
+    end
+
+    private
+
+    def friend_params
+        params.require(:friend).permit(:username, :email)
     end
 end
