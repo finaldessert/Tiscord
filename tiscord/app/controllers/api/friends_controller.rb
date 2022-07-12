2533b2f1b18c774190ba0b50532343b8df5821b3
Ben <finaldessert@gmail.com>
2022-07-12 19:16:19 -0400

    index method / show method for Friends controller added

--- tiscord/app/controllers/api/friends_controller.rb
+++ tiscord/app/controllers/api/friends_controller.rb
@@ -1,2 +1,9 @@
 class Api::FriendsController < ApplicationController
+    def index
+        @friends = Friend.where(:user_id => current_user.id) #we can use this to grab all friends of current user for friends nav bar
+    end
+
+    def create
+        
+    end
 end
